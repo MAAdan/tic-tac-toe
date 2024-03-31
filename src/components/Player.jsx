1c63d9b806aadf254c9dfaef8da124ad57e9f631
Miguel Adan <miguel.adan@kindredgroup.com>
2024-03-31 22:58:05 +0200

    Handle changes on the input element in the Player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,29 @@
-export default function Player({name, symbol}) {
+import { useState } from "react";
+
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(editing => !editing); // => schedules a state update with the latest isEditing value when the state is updated based on the previous state value
+    }
+
+    function handleChange(event) {
+        setPlayerName(event.target.value);
+    }
+
+    // Two-way binding is the action of getting a value out of this input and feeding a value back into this input
+    const editablePlayerName = isEditing ?
+        <input type="text" value={playerName} onChange={handleChange}/> : 
+        <span className="player-name">{playerName}</span>;
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
