56b073e6fb277d74dfc25b3960a33ad4a5241999
Miguel Adan <miguel.adan@kindredgroup.com>
2024-03-31 17:09:53 +0200

    Create game container structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">x</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">o</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   )
 }
 
